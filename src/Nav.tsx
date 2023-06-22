fb8686246b755fbe6e9909bba6dbb5970072be1d
Aastha Sharma <124134149+aastha315@users.noreply.github.com>
2023-06-22 22:05:04 +0530

    ..

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Nav(){
     return(
     <div>
-      <nav style={{ backgroundColor: 'blue' }} className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className={`navbar navbar-expand-lg ${styles['navbar-custom']}`}>
     <div className="container-fluid">
     <a className="navbar-brand" href="#">CD-DEKHLO</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
